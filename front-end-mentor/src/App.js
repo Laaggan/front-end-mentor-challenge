9c3a7d50854dd7d09a9f982a9650ce8923c6d07b
Linus Lagergren <lagergrenlinus@gmail.com>
2021-02-07 17:44:06 +0100

    Made some progress with styling

--- front-end-mentor/src/App.js
+++ front-end-mentor/src/App.js
@@ -7,92 +7,88 @@ import { CheckOutlined, CheckCircleTwoTone } from "@ant-design/icons";
 function App() {
   const [sliderValue, setSliderValue] = useState(50);
   const [discount, setDiscount] = useState(0);
-  
+
   var data = ["Unlimited websites", "100% data ownership", "Email reports"];
 
   const calcDisccount = (discount, price) => {
-    return price - price * discount
-  }
+    return price - price * discount;
+  };
 
   const handleSlider = (e) => {
-    setSliderValue(
-      calcDisccount(discount, e.target.value)
-    );
+    setSliderValue(calcDisccount(discount, e.target.value));
   };
 
   const handleSwitch = (e) => {
     if (discount == 0) {
       setDiscount(0.25);
-      setSliderValue(
-        sliderValue * 0.75
-      );
+      setSliderValue(sliderValue * 0.75);
     } else {
-      setSliderValue(
-        sliderValue / 0.75
-      );
+      setSliderValue(sliderValue / 0.75);
       setDiscount(0);
     }
   };
 
   return (
-    <div className="background">
+    <>
       <div className="App">
         {/* <header className="App-header">
       </header> */}
         <body>
-          <div>
-            <Typography.Title>Simple, traffic-based pricing</Typography.Title>
-            <div className="sub-title">
-              <p>Sign-up for our 30-day trial. No credit card required.</p>
-            </div>
-          </div>
-          <div>
-            <Space>
-              <div className="sub-title" id="price">
-                <Space>
-                  100k Pageviews
-                  <div className="money">{sliderValue}</div>$ /month
-                </Space>
+          <div className="main-app">
+            <div>
+              <Typography.Title>Simple, traffic-based pricing</Typography.Title>
+              <div className="sub-title">
+                <p>Sign-up for our 30-day trial. No credit card required.</p>
               </div>
-            </Space>
-          </div>
-          <div id="slider-container">
-            {/* <Slider
+            </div>
+            <div>
+              <Space>
+                <div className="sub-title" id="price">
+                  <Space>
+                    100k Pageviews
+                    <div className="money">{sliderValue}</div>$ /month
+                  </Space>
+                </div>
+              </Space>
+            </div>
+            <div id="slider-container">
+              {/* <Slider
               style={{ width: 500 }}
               defaultValue={sliderValue}
               onChange={handleSlider}
             /> */}
-            <div className="slidecontainer">
-              <input
-                type="range"
-                min="1"
-                max="100"
-                className="slider"
-                onChange={handleSlider}
-              ></input>
+              <div className="slidecontainer">
+                <input
+                  type="range"
+                  min="1"
+                  max="100"
+                  className="slider"
+                  onChange={handleSlider}
+                ></input>
+              </div>
             </div>
-          </div>
-          <div>
-            <Space>
-              Monthly Billing
-              <Switch onChange={handleSwitch} />
-              Yearly Billing
-              <div className="discount-container">25% discount</div>
-            </Space>
-          </div>
-          <div className="button-container">
-            <Space>
-              <List
-                dataSource={data}
-                renderItem={(item) => (
-                  <List.Item>
-                    {/* <Typography.Text><CheckOutlined twoToneColor="#52c41a" /></Typography.Text> {item} */}
-                    <CheckCircleTwoTone twoToneColor="#52c41a" /> {item}
-                  </List.Item>
-                )}
-              />
-              <Button type="default">Start my trial</Button>
-            </Space>
+            <div>
+              <Space>
+                Monthly Billing
+                <Switch onChange={handleSwitch} />
+                Yearly Billing
+                <div className="discount-container">25% discount</div>
+              </Space>
+            </div>
+            <div className="button-container">
+              <Space>
+                <List
+                  dataSource={data}
+                  renderItem={(item) => (
+                    <List.Item>
+                      {/* <Typography.Text><CheckOutlined twoToneColor="#52c41a" /></Typography.Text> {item} */}
+                      <CheckCircleTwoTone twoToneColor="#52c41a" /> {item}
+                    </List.Item>
+                  )}
+                />
+                <Button type="default">Start my trial</Button>
+              </Space>
+            </div>
           </div>
         </body>
 
@@ -109,7 +105,7 @@ function App() {
           </div>
         </footer>
       </div>
-    </div>
+    </>
   );
 }
 
